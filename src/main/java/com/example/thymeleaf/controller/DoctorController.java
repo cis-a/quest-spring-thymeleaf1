73608b2876185c2735fffc17bcdda41746591b6b
Christoph Abert <christoph.abert@telekom.de>
2019-12-05 11:15:01 +0100

    Doctor Model & Params & Fragments

--- src/main/java/com/example/thymeleaf/controller/DoctorController.java
+++ src/main/java/com/example/thymeleaf/controller/DoctorController.java
@@ -6,14 +6,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 
 public class DoctorController {
 
     @GetMapping("/doctor")
+    //@RequestMapping(value = "/doctor", method = RequestMethod.POST)
 
-    public String doctor(Model mydoctor, @RequestParam(defaultValue = "John Smith") String name, @RequestParam(defaultValue = "0") int number) {
+    public String doctor(Model mydoctor,
+    @RequestParam(defaultValue = "John Smith") String name,
+    @RequestParam(defaultValue = "0") int number) {
       //if name or number are not provided in form, defaults are John Smith and zero
       String doctorname = name;
       mydoctor.addAttribute("name", doctorname);
